b4b6d5d0935485311ba1a4c5fcdea8619ee1a79b
dotMaco <macko.andrej1@gmail.com>
2019-09-28 23:50:14 +0200

    PlatformGenerator created (not finished)

---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour
+{
+
+    public GameObject platform;
+    public float leftWall;
+    public float rigthWall;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   // Random.Range(-10.0f, 10.0f)
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GeneratePlatforms()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+        }
+    }
+
+
+
+}
