dfedcfa49f76a0f324bdc4b7c525d453ff3fa633
dotMaco <macko.andrej1@gmail.com>
2019-09-30 22:40:12 +0200

    Camera goes automaticly up and added Platform spawner and collector

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public Vector3 offset;  
-    public float smoothTime = 0.3F;
-    private Vector3 velocity = Vector3.zero;
+    //public Transform target;
+    //public Vector3 offset;  
+    //public float smoothTime = 0.3F;
+    //private Vector3 velocity = Vector3.zero;
+
+    public Transform _camera;
+    public Transform player;
+    public float speedOfCamera;
 
     private void LateUpdate()
     {
+        float playerHeight = player.position.y;
+
+        if (playerHeight >= 1.3f)
+        {
+            transform.Translate((Vector3.up * (Time.deltaTime * speedOfCamera)), Space.World);
+        }
+
         
-        Vector3 desiredPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
-        transform.position = new Vector3(0f, smoothPosition.y, smoothPosition.z);
+        //Vector3 desiredPosition = target.position + offset;
+        //Vector3 smoothPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothTime);
+        //transform.position = new Vector3(0f, smoothPosition.y, smoothPosition.z);
     }
 }
--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 public class PlatformGenerator : MonoBehaviour
 {
 
-    public GameObject platform;
-    public float leftWall;
-    public float rigthWall;
-    // Start is called before the first frame update
-    void Start()
-    {
+
+    //public GameObject platform;
+    //public float leftWall;
+    //public float rigthWall;
+    //public Vector3 lastPlatformPart;
+    //public float minOffset;
+    //public float maxOffset;
+   
+    //void Start()
+    //{
         
-    }
-   // Random.Range(-10.0f, 10.0f)
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //}
+  
+    //void Update()
+    //{
+    //    InvokeRepeating("GeneratePlatforms", 3f, 1f);
+    //}
 
-    public void GeneratePlatforms()
-    {
-        if (Input.GetKey(KeyCode.Space))
-        {
+    //public void GeneratePlatforms()
+    //{
+    //    Vector3 spawnPosition = Vector3.zero;
 
-        }
-    }
+    //    float newAxisX = Random.Range(leftWall, rigthWall);
+    //    float offsetForY = Random.Range(minOffset, maxOffset);
 
+    //    spawnPosition = new Vector3(newAxisX, lastPlatformPart.y + offsetForY,0f);
 
+    //    GameObject newPlatform = Instantiate(platform, spawnPosition, Quaternion.Euler(0, 0, 0));
 
+    //    lastPlatformPart = newPlatform.transform.position;
+    
+    //}
 }
